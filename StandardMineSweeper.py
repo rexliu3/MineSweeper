2895fdca260e993651ffdbabd80038761d838882
rexliu3 <40341857+rexliu3@users.noreply.github.com>
2020-05-18 23:22:55 -0700

    Test feature functional and board generation works

--- StandardMineSweeper.py
+++ StandardMineSweeper.py
@@ -1,13 +1,13 @@
 from random import randint
 
-board = []
+mainBoard = []
 
 # 20 - 99 / 18 - 40 / 10 - 10
-numRowsColumns = 20
-numMines = 99
+numRowsColumns = 5
+numMines = 10
 
 
-def print_board():
+def print_board(board):
     for i in range(numRowsColumns):
         for j in range(numRowsColumns):
             if j != numRowsColumns - 1:
@@ -17,13 +17,15 @@ def print_board():
 
 
 def set_up():
+    board = []
     for i in range(numRowsColumns):
         board.append([])
         for j in range(numRowsColumns):
             board[i].append(0)
+    return board
 
 
-def place_mines():
+def place_mines(board):
     for k in range(numMines):
         rowCoordinate = randint(0, numRowsColumns - 1)
         columnCoordinate = randint(0, numRowsColumns - 1)
@@ -32,48 +34,84 @@ def place_mines():
             columnCoordinate = randint(0, numRowsColumns - 1)
         board[rowCoordinate][columnCoordinate] = -1
 
-        try:
+        if rowCoordinate != 0 and columnCoordinate != 0:
             if board[rowCoordinate - 1][columnCoordinate - 1] >= 0:
                 board[rowCoordinate - 1][columnCoordinate - 1] += 1
-        except IndexError:
-            pass
-        try:
+
+        if rowCoordinate != 0:
             if board[rowCoordinate - 1][columnCoordinate] >= 0:
                 board[rowCoordinate - 1][columnCoordinate] += 1
-        except IndexError:
-            pass
-        try:
+
+        if rowCoordinate != 0 and columnCoordinate != numRowsColumns - 1:
             if board[rowCoordinate - 1][columnCoordinate + 1] >= 0:
                 board[rowCoordinate - 1][columnCoordinate + 1] += 1
-        except IndexError:
-            pass
-        try:
+
+        if columnCoordinate != 0:
             if board[rowCoordinate][columnCoordinate - 1] >= 0:
                 board[rowCoordinate][columnCoordinate - 1] += 1
-        except IndexError:
-            pass
-        try:
+
+        if columnCoordinate != numRowsColumns - 1:
             if board[rowCoordinate][columnCoordinate + 1] >= 0:
                 board[rowCoordinate][columnCoordinate + 1] += 1
-        except IndexError:
-            pass
-        try:
+
+        if rowCoordinate != numRowsColumns - 1 and columnCoordinate != 0:
             if board[rowCoordinate + 1][columnCoordinate - 1] >= 0:
                 board[rowCoordinate + 1][columnCoordinate - 1] += 1
-        except IndexError:
-            pass
-        try:
+
+        if rowCoordinate != numRowsColumns - 1:
             if board[rowCoordinate + 1][columnCoordinate] >= 0:
                 board[rowCoordinate + 1][columnCoordinate] += 1
-        except IndexError:
-            pass
-        try:
+
+        if rowCoordinate != numRowsColumns - 1 and columnCoordinate != numRowsColumns - 1:
             if board[rowCoordinate + 1][columnCoordinate + 1] >= 0:
                 board[rowCoordinate + 1][columnCoordinate + 1] += 1
-        except IndexError:
-            pass
+    return board
 
 
-set_up()
-place_mines()
-print_board()
+def test_generation(board):
+    for test in range(10000):
+        for rowCoordinate in range(len(board)):
+            for columnCoordinate in range(len(board[0])):
+                if board[rowCoordinate][columnCoordinate] >= 0:
+                    counter = 0
+                    if rowCoordinate != 0 and columnCoordinate != 0:
+                        if board[rowCoordinate - 1][columnCoordinate - 1] < 0:
+                            counter += 1
+
+                    if rowCoordinate != 0:
+                        if board[rowCoordinate - 1][columnCoordinate] < 0:
+                            counter += 1
+
+                    if rowCoordinate != 0 and columnCoordinate != numRowsColumns - 1:
+                        if board[rowCoordinate - 1][columnCoordinate + 1] < 0:
+                            counter += 1
+
+                    if columnCoordinate != 0:
+                        if board[rowCoordinate][columnCoordinate - 1] < 0:
+                            counter += 1
+
+                    if columnCoordinate != numRowsColumns - 1:
+                        if board[rowCoordinate][columnCoordinate + 1] < 0:
+                            counter += 1
+
+                    if rowCoordinate != numRowsColumns - 1 and columnCoordinate != 0:
+                        if board[rowCoordinate + 1][columnCoordinate - 1] < 0:
+                            counter += 1
+
+                    if rowCoordinate != numRowsColumns - 1:
+                        if board[rowCoordinate + 1][columnCoordinate] < 0:
+                            counter += 1
+
+                    if rowCoordinate != numRowsColumns - 1 and columnCoordinate != numRowsColumns - 1:
+                        if board[rowCoordinate + 1][columnCoordinate + 1] < 0:
+                            counter += 1
+                    if counter != board[rowCoordinate][columnCoordinate]:
+                        return False
+    return True
+
+
+mainBoard = set_up()
+sub = place_mines(mainBoard)
+mainBoard = sub
+print_board(mainBoard)
+print(test_generation(mainBoard))
